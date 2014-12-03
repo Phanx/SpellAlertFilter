d5fdc4781869470ecbade4d5ed4529d884a21bb0
Phanx <addons@phanx.net>
2014-12-03 09:05:28 -0800

    Rename setup command

--- Addon.lua
+++ Addon.lua
@@ -17,15 +17,15 @@ if GetLocale() == "deDE" then
 	L["All spell alerts have been removed from the filter list."] = "Alle Zauberwarnmeldungen wurden aus der Verstecksliste entfernt."
 	L["disabled"] = "deaktiviert"
 	L["enabled"] = "aktiviert"
-	L["Setup mode is now %s."] = "Der Konfigurationsmodus ist jetzt %s."
+	L["Spell alert reporting now %s."] = "Berichten über Zauberwarnmeldungen wird jetzt %s."
 elseif GetLocale():match("^es") then
 	L["%d (%s) will no longer be hidden."] = "%d (%s) ya no se oculta."
 	L["%d (%s) will now be hidden."] = "%d (%s) ahora se oculta."
 	L["%d spell alerts are being hidden."] = "%d alertas de hechizos se ocultan actualmente."
 	L["All spell alerts have been removed from the filter list."] = "Todos hechizos se han eliminados por la lista para ocultar."
-	L["disabled"] = "desactivado"
-	L["enabled"] = "activado"
-	L["Setup mode is now %s."] = "El modo de configuración está ahora %s."
+	L["disabled"] = "desactivados"
+	L["enabled"] = "activados"
+	L["Spell alert reporting now %s."] = "Informes sobre alertas de hechizos se han %s."
 end
 
 L["enabled"] = "|cff7fff7f" .. L["enabled"] .. "|r"
@@ -67,15 +67,15 @@ local options = {
 			DEFAULT_CHAT_FRAME:AddMessage("   " .. format(L["%d (%s)"], spellID, GetSpellInfo(spellID) or UNKNOWN))
 		end
 	end,
+	loud = function()
+		setupMode = not setupMode
+		DEFAULT_CHAT_FRAME:AddMessage("|cff7fff7fSpellAlertFilter:|r " .. format(L["Spell alert reporting now %s."], setupMode and L["enabled"] or L["disabled"]))
+	end,
 	reset = function()
 		for id in pairs(SpellAlertFilter) do
 			SpellAlertFilter[id] = nil
 		end
 		DEFAULT_CHAT_FRAME:AddMessage("|cff7fff7fSpellAlertFilter:|r " .. L["All spell alerts have been removed from the filter list."])
-	end,
-	setup = function()
-		setupMode = not setupMode
-		DEFAULT_CHAT_FRAME:AddMessage("|cff7fff7fSpellAlertFilter:|r " .. format(L["Setup mode is now %s."], setupMode and L["enabled"] or L["disabled"]))
 	end,
 }
 
@@ -98,7 +98,7 @@ SlashCmdList.SPELLALERTFILTER = function(cmd)
 		DEFAULT_CHAT_FRAME:AddMessage("|cff7fff7fSpellAlertFilter:|r " .. L["Use /saf with the following commands:"])
 		DEFAULT_CHAT_FRAME:AddMessage("   12345 - Add/remove spell ID 123456 to/from the filter list.")
 		DEFAULT_CHAT_FRAME:AddMessage("   list - List the spell alerts currently on the filter list.")
+		DEFAULT_CHAT_FRAME:AddMessage("   loud - Show a message in the chat frame with the related spell ID whenever a spell alert is displayed.")
 		DEFAULT_CHAT_FRAME:AddMessage("   reset - Remove all spell alerts from the filter list.")
-		DEFAULT_CHAT_FRAME:AddMessage("   setup - Show a message in the chat frame with the related spell ID whenever a spell alert is displayed.")
 	end
 end
